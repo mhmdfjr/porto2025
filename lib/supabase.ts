02d81f74a4b4bddff0884e8103618338943f31bf
mhmdfjr <moh.fajar1304@gmail.com>
2025-07-24 00:38:16 +0700

    feat: env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://lgklimjczxflxpmtjsoi.supabase.co"
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxna2xpbWpjenhmbHhwbXRqc29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNTAyNjcsImV4cCI6MjA2ODgyNjI2N30.PLLE2BOTY0HrezAhG6GomaqjbL1iWHeTJdPnHnWRgZg"
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 // Types for your database tables
